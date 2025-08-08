5db78831f8d972838fd60c475d83458ea9510031
esdrascosta <esdras.cts@outlook.com>
2025-08-07 23:57:03 -0300

    feat: criado as actions para o crud de clientes

--- api.crud-cliente/src/Controllers/ClientesController.cs
+++ api.crud-cliente/src/Controllers/ClientesController.cs
@@ -1,5 +1,6 @@
 
 using api.crud_cliente.src.Models;
+using api.crud_cliente.src.Service.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.crud_cliente.src.Controllers
@@ -8,11 +9,59 @@ namespace api.crud_cliente.src.Controllers
     [Route("api/[controller]")]
     public class ClientesController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<ClienteResposta>>> GetTodosClientes()
+        private readonly IClienteService _clienteService;
+
+        public ClientesController(IClienteService clienteService)
         {
-            var teste = new ClienteResposta();
-            return Ok(teste);
+            _clienteService = clienteService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ClienteResposta>>> ObterTodosClientes()
+        {
+            var clientes = await _clienteService.GetTodosClientesAsync();
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClienteResposta>> ObterClientePorId(Guid id)
+        {
+            var cliente = await _clienteService.GetClientePorIdAsync(id);
+
+            if (cliente == null)
+                return NotFound();
+
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClienteResposta>> CriarCliente([FromBody] ClienteRequisicao clienteRequisicao)
+        {
+            var novoCliente = await _clienteService.CriarClienteAsync(clienteRequisicao);
+
+            return CreatedAtAction(nameof(ObterClientePorId), new { id = novoCliente.Id }, novoCliente);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarCliente(Guid id, [FromBody] ClienteRequisicao clienteRequisicao)
+        {
+            var clienteAtualizado = await _clienteService.AtualizarClienteAsync(id, clienteRequisicao);
+
+            if (clienteAtualizado == false)
+                return NotFound();
+
+            return Ok(clienteAtualizado);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarCliente(Guid id)
+        {
+            var clienteDeletado = await _clienteService.RemoverClienteAsync(id);
+
+            if (clienteDeletado == false)
+                return NotFound();
+
+            return NoContent();
         }
     }
 }
