bd3f43a4bc483e42dc2973e7c6bc082ab466266d
esdrascosta <esdras.cts@outlook.com>
2025-08-07 23:35:38 -0300

    feat: adicionado um dto para os dados recebidos na requsiicao da api

--- api.crud-cliente/src/Models/Cliente.cs
+++ api.crud-cliente/src/Models/Cliente.cs
@@ -18,3 +18,14 @@ public class ClienteResposta
     public string Email { get; set; }
     public string Celular { get; set; }
 }
+
+public class ClienteRequisicao
+{
+    public required Guid Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Sobrenome { get; set; }
+    public required string CPF { get; set; }
+    public required string Email { get; set; }
+    public required string Celular { get; set; }
+    public required decimal RendaMensal { get; set; }
+}
