128dfd5ad84cb1839a0340ab4cc2996a4140eaff
esdrascosta <esdras.cts@outlook.com>
2025-08-08 10:28:18 -0300

    feat: adicionando as configuracoes do ef

--- console.cartao-credito/Program.cs
+++ console.cartao-credito/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Microservico Cartao de Credito!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using console.cartao_credito.src.Data;
+using console.cartao_credito.src.Evento.Consumer;
+using Microsoft.Extensions.Configuration;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(connectionString));
+
+    });
+
+var host = builder.Build();
+
+var consumer = host.Services.GetRequiredService<RabbitMQCartaoConsumer>();
+consumer.Start();
+
+await host.RunAsync();
