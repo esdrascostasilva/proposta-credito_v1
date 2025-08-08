87a21260038f351306a77024c1669fcab580b1c1
esdrascosta <esdras.cts@outlook.com>
2025-08-08 08:48:00 -0300

    feat: registros de servicos

--- console.analise-credito/Program.cs
+++ console.analise-credito/Program.cs
@@ -1,7 +1,22 @@
-﻿
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using console.analise_credito.src.Data;
 using console.analise_credito.src.Evento.Consumer;
+using Microsoft.Extensions.Configuration;
 
-var consumer = new RabbitMQEventoConsumer();
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddSingleton<RabbitMQEventoConsumer>();
+    })
+    .Build();
+
+var consumer = host.Services.GetRequiredService<RabbitMQEventoConsumer>();
 consumer.Start();
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Aplicacao em execucao. Pressione qualquer tecla para sair...");
+Console.ReadKey();
