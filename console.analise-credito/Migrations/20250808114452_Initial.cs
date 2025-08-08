920f55a2762283c2a6a1fd82eafbdf072717c378
esdrascosta <esdras.cts@outlook.com>
2025-08-08 08:50:24 -0300

    feat: migrations

---
+++ console.analise-credito/Migrations/20250808114452_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace console.analise_credito.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AvaliacoesClientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RendaMensal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ValorCreditoOferecido = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    estaElegivel = table.Column<bool>(type: "bit", nullable: false),
+                    AvalidoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AvaliacoesClientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AvaliacoesClientes");
+        }
+    }
+}
