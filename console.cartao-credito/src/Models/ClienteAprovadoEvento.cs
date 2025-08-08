1e421fe35aefd5b1a0cc8ff6a17ac2d06f0a75be
esdrascosta <esdras.cts@outlook.com>
2025-08-08 09:54:59 -0300

    feat: criacao das models dos dados lidos do rabbitMQ e dos dados que serao salvos no bd

---
+++ console.cartao-credito/src/Models/ClienteAprovadoEvento.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace console.cartao_credito.src.Models;
+
+public class ClienteAprovadoEvento
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public string Email { get; set; }
+    public decimal RendaMensal { get; set; }
+    public decimal ValorCreditoAprovado { get; set; }
+}
+
+public class ClienteCratoaCredito
+{
+    public required Guid Id { get; set; }
+    public required string NomeTitular { get; set; }
+    public required string Numero { get; set; }
+    public required string Validade { get; set; }
+    public required string Cvv { get; set; }
+    public required decimal Limite { get; set; }
+    public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+}
