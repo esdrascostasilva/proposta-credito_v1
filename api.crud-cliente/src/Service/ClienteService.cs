e19866abb2a9d54b981e6c6e416813cc61e00d69
esdrascosta <esdras.cts@outlook.com>
2025-08-14 06:58:44 -0300

    feat: criando o metodo de servico para o aceite de credito

--- api.crud-cliente/src/Service/ClienteService.cs
+++ api.crud-cliente/src/Service/ClienteService.cs
@@ -3,6 +3,8 @@ using api.crud_cliente.src.Data;
 using api.crud_cliente.src.Evento.Interface;
 using api.crud_cliente.src.Models;
 using api.crud_cliente.src.Service.Interface;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.crud_cliente.src.Service;
@@ -109,6 +111,31 @@ public class ClienteService : IClienteService
 
         _context.Clientes.Remove(cliente);
         await _context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<bool> AceitarPropostaAsync(Guid id)
+    {
+        // TODO: checar o detalhes e finalizar depois
+        // pegar o Id e procurear no banco de dados
+        var cliente = await _context.Clientes.FindAsync(id);
+
+        if (cliente == null)
+            return false;
+
+        // se existir, alterar a flag(acho q ano tenho essa coluna status, preciso criar) de status e escreve para o broker
+        // cliente.DesejaCartaoCredito = true;
+
+        // enviando a mensagem para o broker. Verificar a model que preciso passar (checar no worker de cartao-credito)
+        // _eventBus.Publish(new ClienteCriadoEvento
+        // {
+        //     Id = cliente.Id,
+        //     Nome = cliente.Nome,
+        //     CPF = cliente.CPF,
+        //     Email = cliente.Email,
+        //     RendaMensal = cliente.RendaMensal
+        // }, "clientes.elegiveis"); // TODO: trocar o nome da fila? Maybe
 
         return true;
     }
